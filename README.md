c15dc73e0a8c06838d6de6cb7b1fe5a70bcbe754
Hans Saurer <hans@saurer.name>
2014-12-29 14:16:20 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## PX4 Test Suite for Continuous Delivery
+
+*   Official Website: http://px4.io
+
+##Initial 
+###Separate scripts for Clone+Build and running tests
---
+++ build.rb
@@ -0,0 +1,94 @@
+require 'sinatra'
+require 'json'
+require 'octokit'
+
+#You can write Visual-Basic in any language!
+
+
+$srcdir = "./testsrc"            #From some kind of config - later
+
+$ACCESS_TOKEN = ENV['GITTOKEN']
+
+    
+def do_clone (branch)
+    puts "do_clone: " + branch
+    system 'mkdir', '-p', $srcdir
+    Dir.chdir($srcdir) do
+        #git clone <url> --branch <branch> --single-branch [<folder>]
+        result = `git clone https://github.com/hanssaurer/Firmware.git --branch #{branch} --single-branch `
+        puts result
+        Dir.chdir("./Firmware") do
+            result = `git clone https://github.com/PX4/NuttX`
+            puts result
+        end
+    end
+end    
+    
+def do_build ()
+    puts "build"
+    Dir.chdir($srcdir+"/Firmware") do
+        result = `git submodule init`
+        puts result
+        result = `git submodule update`
+        puts result
+        result = `git submodule status`
+        puts result
+        result = `make distclean`
+        puts result
+        result = `make archives`
+        puts result
+        result = `make`
+        puts result
+        result = `make upload px4fmu-v2_default`
+        puts result
+    end
+end    
+
+def set_PR_Status (prstatus)
+    puts "Access token: " + $ACCESS_TOKEN
+    client = Octokit::Client.new(:access_token => $ACCESS_TOKEN)
+    #puts client.user.location
+    #puts pr['base']['repo']['full_name']
+    #puts pr['head']['sha']
+    client.create_status(pr['base']['repo']['full_name'], pr['head']['sha'], prstatus)
+    puts "fertig!"
+end    
+
+
+# ---------- Routing ------------
+get '/' do
+  'Hallo unbekannt'
+end
+post '/payload' do
+
+    body = JSON.parse(request.body.read)
+    puts "I got some JSON: " + JSON.pretty_generate(body)
+    puts "Envelope: " + JSON.pretty_generate(request.env)
+
+    github_event = request.env['HTTP_X_GITHUB_EVENT']
+    puts "Event: " + github_event
+    case github_event
+    when 'ping'
+        "Hello"    
+    when 'pull_request'
+        #puts body.keys
+        pr = body["pull_request"]
+        branch = pr["base"]["ref"]
+        a = branch.split('/')
+        branch = a[a.count-1]           #last part is the bare branchname
+        puts "Going to clone branch: " + branch
+        #do_clone  branch
+        #do_build
+        set_PR_Status('success')
+    when 'push'
+        branch = body['ref']
+        a = branch.split('/')
+        branch = a[a.count-1]           #last part is the bare branchname
+        puts "Going to clone branch: " + branch
+        do_clone  branch
+        do_build
+
+    else
+        puts "unknown event"
+    end
+end
