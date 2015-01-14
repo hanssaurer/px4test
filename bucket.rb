e64c5cc40d6c9e91d785e9431d3323fd975de06f
Lorenz Meier <lm@inf.ethz.ch>
2015-01-14 10:49:06 +0100

    Example how to upload to an AWS bucket

---
+++ bucket.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'aws-sdk'
+
+bucket_name = 'px4-travis'
+file_name = 'test.txt'
+
+File.open(file_name, 'w') {|f| f.write(Time.now.strftime("%d/%m/%Y %H:%M")) }
+
+# Get an instance of the S3 interface.
+s3 = AWS::S3.new
+
+# Upload a file.
+key = File.basename(file_name)
+s3.buckets[bucket_name].objects[key].write(:file => file_name)
+puts "Uploading file #{file_name} to bucket #{bucket_name}."
