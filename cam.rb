0333f1f36308e1e833e1fd47f8378ab35a596f2f
Lorenz Meier <lm@inf.ethz.ch>
2015-01-17 16:22:43 +0100

    camera: Create smoother animation

--- cam.rb
+++ cam.rb
@@ -9,7 +9,7 @@ def take_picture(workingdir)
   capture = Webcam.new(0)
 
   # Capture 30 frames
-  for i in 0..30
+  for i in 0..45
     image = capture.grab
     image.save "%s/new_image%d.jpg" % [workingdir, i]
   end
@@ -17,7 +17,7 @@ def take_picture(workingdir)
 
   # Create a GIF using these frames
   animation = ImageList.new(*Dir["*.jpg"])
-  animation.delay = 10
+  animation.delay = 20
   animation.write("%s/animated.gif" % workingdir)
   FileUtils.rm_rf(Dir.glob("%s/*.jpg" % workingdir))
   return true
